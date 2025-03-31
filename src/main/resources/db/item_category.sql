a760939dc9030ba59e0c1146ce4d6c9425455d5a
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2025-03-31 19:41:10 +0200

    Initial commit

---
+++ src/main/resources/db/item_category.sql
@@ -0,0 +1,12 @@
+INSERT INTO item_category (name, description)
+VALUES
+    ('Klasa 1.', 'Materiały przeznaczone dla uczniów 1. klasy szkoły podstawowej.'),
+    ('Klasa 2.', 'Materiały przeznaczone dla uczniów 2. klasy szkoły podstawowej.'),
+    ('Klasa 3.', 'Materiały przeznaczone dla uczniów 3. klasy szkoły podstawowej.'),
+    ('Klasa 4.', 'Materiały przeznaczone dla uczniów 4. klasy szkoły podstawowej.'),
+    ('Klasa 5.', 'Materiały przeznaczone dla uczniów 5. klasy szkoły podstawowej.'),
+    ('Klasa 6.', 'Materiały przeznaczone dla uczniów 6. klasy szkoły podstawowej.'),
+    ('Klasa 7.', 'Materiały przeznaczone dla uczniów 7. klasy szkoły podstawowej.'),
+    ('Klasa 8.', 'Materiały przeznaczone dla uczniów 8. klasy szkoły podstawowej.'),
+    ('Technikum/Liceum', 'Materiały przeznaczone dla uczniów szkół wyższych'),
+    ('Dla dorosłych', 'Materiały przeznaczone dla osób dorosłych.'),
