896109d8970a141d112529f4a8c26fd7d952834e
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2025-04-01 20:55:29 +0200

    Added Cart + CartItem entity

---
+++ src/main/java/pl/klugeradoslaw/backendshop/Cart/CartService.java
@@ -0,0 +1,7 @@
+package pl.klugeradoslaw.backendshop.Cart;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartService {
+}
---
+++ src/main/java/pl/klugeradoslaw/backendshop/CartItem/CartItemController.java
@@ -0,0 +1,8 @@
+package pl.klugeradoslaw.backendshop.CartItem;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CartItemController {
+
+}
