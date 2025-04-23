36338bf3f5f9895e1bf74d3bd9f2748f0757efce
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2025-04-23 22:11:58 +0200

    liqubase config, added item_category + product table

---
+++ src/main/resources/db/testdata/product.sql
@@ -0,0 +1,3 @@
+INSERT INTO product (name, description, price, item_category_id, photoUrl, fileUrl, createdAt)
+VALUES
+    ('Egzamin 8 klasisty z Harrym Potterem', 'Egzamin 8 klasisy dla uczniów szkół podstawowych.', '17.00', 9, 'photo url 1', 'file url 1', '2025-03-03T10:15:30' );
